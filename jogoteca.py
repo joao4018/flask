312f61561c85ada8b73aebd46b5c401847e776ed
joao4018 <joao4018@gmail.com>
2022-01-21 17:57:43 -0300

    Connect to DB

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, flash, session, url_for
+from flask import Flask, render_template
 
 from database import usuario1, usuario2, usuario3, jogo2, jogo1
-from model.Jogo import Jogo
 from services import JogoService, UserService
 
 app = Flask(__name__)
@@ -12,11 +11,11 @@ usuarios = {usuario1.id: usuario1,
             usuario3.id: usuario3}
 lista = [jogo1, jogo2]
 
-
 @app.route('/')
 def index():
     return render_template('lista.html', titulo='Jogos',
                            jogos=lista)
+
 @app.route('/novo')
 def novo():
     return JogoService.novo()
